1ae089ca1ab29daf75f8cb19bae17a2335d80b8c
Jayant Parashar <parasharjayant10.com>
2024-07-15 13:08:46 +0530

    Resolved merge conflicts

--- todoproject/todo/urls.py
+++ todoproject/todo/urls.py
@@ -1,13 +1,13 @@
 # todo/urls.py
 from django.urls import path
-from .views import TodoListView, TodoDetailView, TodoCreateView, TodoUpdateView, TodoDeleteView
+from .views import TodoListView, TodoDetailView, TodoCreateView, TodoUpdateView, TodoDeleteView, signin, signup
 
 urlpatterns = [
     path('', TodoListView.as_view(), name='todo_list'),
     path('todo/<int:pk>/', TodoDetailView.as_view(), name='todo_detail'),
     path('todo/new/', TodoCreateView.as_view(), name='todo_create'),
     path('todo/<int:pk>/edit/', TodoUpdateView.as_view(), name='todo_edit'),
-    path('signup',views.signup, name='signup'),
+    path('signup', signup.as_view(), name='signup'),
     path('signin',views.signin, name='signin'),
     path('todo/<int:pk>/delete/', TodoDeleteView.as_view(), name='todo_delete'),
 ]
